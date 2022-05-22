837fc5c3140378ce374c4af9768ff77e789a591c
Abhishek <sachdevaabu30@gmail.com>
2022-05-23 02:26:07 +0530

    m

--- Lecture-5/fibonacci.cpp
+++ Lecture-5/fibonacci.cpp
@@ -3,16 +3,19 @@ using namespace std;
 
 int main()
 {
-    int sum = 0, n;
+    int a = 0, b = 1;
+    int sum = a+b, n;
     cout << "Enter a number :";
     cin >> n;
+    cout << a << " ";
+    cout << b << " ";
 
-    for (int a = 0, b = 1; sum <= n; sum = a + b)
+    for (int i = 3; i <= n; i++)
     {
         cout << sum << " ";
-
         a = b;
         b = sum;
+        sum=a+b;
     }
     return 0;
 }
